b239bce4cc54502aa786ff5ba216e4fb74e2f0fe
solo5star <solo_5@naver.com>
2022-01-12 14:14:51 +0000

    Remove video hardware codes

--- printers/urls.py
+++ printers/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import PrinterViewSet, RequestmentViewSet, VideoViewSet
+from .views import PrinterViewSet, RequestmentViewSet
 
 
 router = routers.DefaultRouter()
@@ -9,5 +9,4 @@ router.register('requestments', RequestmentViewSet, basename='requestment')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('video/<int:video_id>', VideoViewSet, name="video"),
 ]
